a68c603440f5ec2a7d43eb3909fe76c2f844f152
johannes <73963446+coppeneur@users.noreply.github.com>
2023-05-04 21:47:44 +0200

    geeksforgeeks

--- gropro/src/main/java/coppeneur/johannes/Main.java
+++ gropro/src/main/java/coppeneur/johannes/Main.java
@@ -9,22 +9,22 @@ import coppeneur.johannes.io.WriteFile;
 import java.io.File;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
 
     // lets test reading files
+    // https://www.geeksforgeeks.org/the-knights-tour-problem/
 
-    try{
-        String path = "src/main/resources/test.txt";
-        Input readFile = new ReadFile(path);
-        Measurement measurement = readFile.readInput(path);
+    try {
+      String path = "src/main/resources/test.txt";
+      Input readFile = new ReadFile(path);
+      Measurement measurement = readFile.readInput(path);
       System.out.println(measurement);
       Output writeFile = new WriteFile();
       writeFile.writeFile(measurement);
 
-    }catch (Exception e){
-        System.out.println("In main: \n"+  e.toString());
+    } catch (Exception e) {
+      System.out.println("In main: \n" + e.toString());
     }
-
   }
 }
