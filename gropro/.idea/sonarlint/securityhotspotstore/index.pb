8a747e5c45398b0d828f001eff22d7ce68fd535f
johannes <73963446+coppeneur@users.noreply.github.com>
2023-05-06 17:34:14 +0200

    fixed project structure

---
+++ gropro/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+_
+/src/main/java/coppeneur/johannes/io/Output.java,a\a\aa6f083b7fda997bb89f1695b0064350fab8df64
--- gropro/src/main/java/coppeneur/johannes/Main.java
+++ gropro/src/main/java/coppeneur/johannes/Main.java
@@ -9,22 +9,64 @@ import coppeneur.johannes.io.WriteFile;
 import java.io.File;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+    private static final String OUTPUT_FILE_PATH_DEFAULT = "output/output.txt";
 
-    // lets test reading files
-    // https://www.geeksforgeeks.org/the-knights-tour-problem/
+    private static String OUTPUT_FILE_PATH;
+    private static String INPUT_FILE_PATH;
 
-    try {
-      String path = "src/main/resources/test.txt";
-      Input readFile = new ReadFile(path);
-      Measurement measurement = readFile.readInput(path);
-      System.out.println(measurement);
-      Output writeFile = new WriteFile();
-      writeFile.writeFile(measurement);
+    private static void handleArgs(String[] args) {
 
-    } catch (Exception e) {
-      System.out.println("In main: \n" + e.toString());
+        // number handeling
+
+        //    int firstArg;
+        //    if (args.length > 0) {
+        //      try {
+        //        firstArg = Integer.parseInt(args[0]);
+        //      } catch (NumberFormatException e) {
+        //        System.err.println("Argument" + args[0] + " must be an integer.");
+        //        System.exit(1);
+        //      }
+        //    }
+
+        // input output file
+        if (args == null || args.length == 0) {
+            throw new RuntimeException("No input file declared");
+
+        } else if (args.length == 1) {
+            System.out.println("1 input params");
+            // input file
+            // output file default
+            INPUT_FILE_PATH = args[0];
+            OUTPUT_FILE_PATH = OUTPUT_FILE_PATH_DEFAULT;
+
+        } else if (args.length == 2) {
+            System.out.println("2 input params");
+            INPUT_FILE_PATH = args[0];
+            OUTPUT_FILE_PATH = args[1];
+        }
     }
-  }
+
+    /**
+     * Main function to start the programm
+     *
+     * @param args [0] input File name [1] output file name. default: TODO
+     */
+    public static void main(String[] args) {
+        try {
+            File test = new File("hs.produktion");
+            System.out.println(test.getAbsolutePath());
+            handleArgs(new String[]{"src/main/resources/test.txt"});
+            // lets test reading files
+            // https://www.geeksforgeeks.org/the-knights-tour-problem/
+            // String path = "src/main/resources/test.txt";
+            Input readFile = new ReadFile(INPUT_FILE_PATH);
+            Measurement measurement = readFile.readInput();
+            System.out.println(measurement);
+            Output writeFile = new WriteFile(OUTPUT_FILE_PATH);
+            writeFile.writeFile(measurement);
+
+        } catch (Exception e) {
+            System.out.println("In main: \n" + e.toString());
+        }
+    }
 }
